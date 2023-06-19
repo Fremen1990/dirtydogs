7a449319fd51c8be5f45aeedec5e6360034a9bc7
Tomasz Stanisz <fremen1990@gmail.com>
2023-06-19 11:48:35 +0200

    :art: First draft of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import workInProgress from './assets/wip.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div    className="container">
+            <h1 className="title">Dirty Dogs - Hotel dla Psów </h1>
+            <div className="card">
+                <a href="https://airtable.com/shrJtGBKt2CepfNyg" target="_blank">
+            <img src={workInProgress} className="logo" alt="Pix"/>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <div>
+                    <button className="button">
+                        <h2>Zarezerwuj hotel</h2>
+                    </button>
+            </div>
+                </a>
+            </div>
+        </div>
+    )
 }
 
 export default App
